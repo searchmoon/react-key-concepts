ad8087721e093a6d4032beef1579e636306a66a8
Rumi <moonding901212@gmail.com>
2023-12-14 16:18:36 +0900

    컴포넌트 분리하기

---
+++ src/components/Concept/Concept.jsx
@@ -0,0 +1,11 @@
+const Concept = ({item}) => {
+  return (
+    <li className="concept">
+      <img src={item.image} alt={item.title} />
+      <h2>{item.title}</h2>
+      <p>{item.description}</p>
+    </li>
+  );
+};
+
+export default Concept;
